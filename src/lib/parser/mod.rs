c460f05d17d0182a5ca6191a3e32074ed2bbae57
shigedangao <marc.inthaamnouay@gmail.com>
2020-12-26 15:46:31 +0100

    finish to parse basic information

--- src/lib/parser/mod.rs
+++ src/lib/parser/mod.rs
@@ -2,11 +2,52 @@ use std::collections::BTreeMap;
 use toml::Value;
 use super::helper::error::LError;
 use super::helper::toml::get_value_for_t;
+use super::helper::conv::Convert;
 
-#[derive(Debug)]
+// Constant
+const SPLIT_DELIMITER: &str = "::";
+
+/// Kind
+///
+/// # Description
+/// Kind of toml file
+/// - Workload => workload::{kubernetes workfload} i.e workload::deployment
+/// - Network => network::{kubernetes network object} i.e: network::service
+#[derive(Debug, PartialEq)]
 pub enum Kind {
     Workload(String),
-    Network(String)
+    Network(String),
+    None
+}
+
+impl Convert for Kind {
+    fn convert(v: &Value) -> Self {
+        let kind = get_value_for_t::<String>(v, "kind");
+        if kind.is_err() {
+            return Kind::None;
+        }
+
+        // split the type by using the character '::'
+        let kind = kind.unwrap();
+        let kind = kind
+            .split(SPLIT_DELIMITER)
+            .collect::<Vec<&str>>();
+
+        let t = kind.get(0);
+        let arg = kind.get(1)
+            .unwrap_or_else(|| &"")
+            .to_string();
+
+        if t.is_none() {
+            return Kind::None;
+        }
+
+        match t.unwrap().to_lowercase().as_str() {
+            "workload" => Kind::Workload(arg),
+            "network" => Kind::Network(arg),
+            _ => Kind::None
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -34,10 +75,11 @@ pub fn get_parsed_objects(tmpl: &str) -> Result<Object, LError> {
     };
 
     let name = get_value_for_t::<String>(&ast, "name")?;
+    let kind = Kind::convert(&ast);
     let metadata = get_value_for_t::<BTreeMap<String, String>>(&ast, "metadata")?;
 
     Ok(Object {
-        kind: Kind::Workload("foo".to_string()),
+        kind,
         name,
         metadata
     })
@@ -62,5 +104,6 @@ mod test {
         let object = object.unwrap();
         assert_eq!(object.name, "rusty");
         assert_eq!(object.metadata.get("tier").unwrap(), "backend");
+        assert_eq!(object.kind, super::Kind::Workload("deployment".to_owned()))
     }
 }
