31b35a601b2e49bcf7e4f99d661fa7e98c3c12b0
shigedangao <marc.inthaamnouay@gmail.com>
2021-04-19 12:44:42 +0200

    add support to output in multiple files

--- src/cli/generate/mod.rs
+++ src/cli/generate/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use clap::ArgMatches;
 use crate::cli::helper::error::{
     CError,
@@ -10,6 +11,7 @@ use crate::kube;
 // Constant
 const ARG_PATH: &str = "path";
 const ARG_OUTPUT: &str = "output";
+const ARG_MERGE: &str = "merge";
 
 /// Run
 ///
@@ -26,22 +28,33 @@ pub fn run(args: &ArgMatches) -> Result<(), CError> {
         .ok_or_else(|| CError::from(TypeError::MissingArg(ARG_PATH.to_owned())))?;
 
     let output = args.value_of(ARG_OUTPUT);
+    let merge = args.is_present(ARG_MERGE);
 
     let templates = io::read_files_to_string(path)?;
-    let mut generated_yaml = Vec::new();
+    let mut generated_yaml = HashMap::new();
 
-    for tmpl in templates {
+    for (name, tmpl) in templates {
         let res = parser::get_parsed_objects(tmpl.as_str())
             .map_err(|err| CError::from(TypeError::Lib(err.to_string())))?;
 
         let yaml = kube::generate_yaml(res)
             .map_err(|err| CError::from(TypeError::Lib(err.to_string())))?;
 
-        generated_yaml.push(yaml);
+        generated_yaml.insert(name, yaml);
     }
 
-    let stitched_yaml = generated_yaml.join("");
+    let stitched_yaml = generated_yaml
+        .clone()
+        .into_iter()
+        .map(|(_, v)| v)
+        .collect::<Vec<String>>()
+        .join("");
+
     if let Some(output_path) = output {
+        if merge {
+            return io::write_multiple_files(output_path, generated_yaml);
+        }
+
         return io::write_file(output_path, &stitched_yaml);
     }
 
--- src/cli/helper/io.rs
+++ src/cli/helper/io.rs
@@ -1,5 +1,12 @@
 use std::fs;
+use std::collections::HashMap;
+use std::path::PathBuf;
 use crate::cli::helper::error::CError;
+use super::error::TypeError;
+
+// Error constant
+const NOT_DIR: &str = "Path is not a directory";
+const NO_YAML: &str = "Path is not a .yaml file";
 
 /// Read Files To String
 ///
@@ -10,9 +17,9 @@ use crate::cli::helper::error::CError;
 /// * `path` - &str
 ///
 /// # Return
-/// * `Result<Vec<String>, CError>`
-pub fn read_files_to_string(path: &str) -> Result<Vec<String>, CError> {
-    let mut templates = Vec::new();
+/// * `Result<HashMap<String, String>, CError>`
+pub fn read_files_to_string(path: &str) -> Result<HashMap<String, String>, CError> {
+    let mut templates = HashMap::new();
     let dir = fs::read_dir(path)?;
 
     for entry in dir {
@@ -20,9 +27,10 @@ pub fn read_files_to_string(path: &str) -> Result<Vec<String>, CError> {
 
         let path = entry.path();
         if path.is_file() {
-            let tmpl = fs::read_to_string(path)?;
-
-            templates.push(tmpl);
+            if let Some(name) = path.clone().file_stem().and_then(|u| u.to_str()) {
+                let tmpl = fs::read_to_string(path)?;
+                templates.insert(name.to_owned(), tmpl);
+            }
         }
     }
 
@@ -41,6 +49,43 @@ pub fn read_files_to_string(path: &str) -> Result<Vec<String>, CError> {
 /// # Return
 /// Result<(), CError>
 pub fn write_file(path: &str, contents: &str) -> Result<(), CError> {
+    // Check that the filename is at least a yaml
+    let p = PathBuf::from(path);
+    if let Some(ext) = p.extension() {
+        if !ext.eq("yaml") {
+            return Err(CError::from(TypeError::Io(NO_YAML.to_owned())));
+        }
+    }
+
     fs::write(path, contents)
         .map_err(|err| CError::from(err))
+}
+
+/// Write Multiple Files
+///
+/// # Description
+/// Write the contents on multiple files
+///
+/// # Arguments
+/// * `path` - &str
+/// * `map` - HashMap<String, String>
+///
+/// # Return
+/// Result<(), CError>
+pub fn write_multiple_files(path: &str, map: HashMap<String, String>) -> Result<(), CError> {
+    // Check that the path is a directory
+    let p = PathBuf::from(path);
+    if !p.is_dir() {
+        return Err(CError::from(TypeError::Io(NOT_DIR.to_owned())));
+    } 
+
+    for (name, content) in map {
+        let mut concat_path = PathBuf::from(path);
+        concat_path.push(format!("{}.yaml", name));
+
+        fs::write(concat_path, content)
+            .map_err(|err| CError::from(err))?;
+    }
+
+    Ok(())
 }
