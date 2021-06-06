15e352fc1171649ce68c8f021b6961f60b0a23b9
shigedangao <marc.inthaamnouay@gmail.com>
2021-06-06 21:47:23 +0200

    improve help command by adding examples. No input print the help

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,18 @@ mod kube;
 
 use clap::{App, load_yaml};
 use termion::color;
+use cli::helper::error::CError;
 
 fn main() {
     let yaml = load_yaml!("cli.yaml");
-    let matches = App::from(yaml).get_matches();
+    let mut app = App::from(yaml);
+    let matches = app.clone().get_matches();
 
     let res = match matches.subcommand() {
         Some(("generate", args)) => cli::generate::run(args),
         Some(("diff", args)) => cli::diff::run(args),
-        _ => Ok(())
+        _ => app.print_help()
+                .map_err(|err| CError::from(err))
     };
 
     if let Err(err) = res {
